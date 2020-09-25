54e14780ee8161d29ec9e921a1e0403ed9225e16
Sergio Perez-Valentin <sergiopv02@gmail.com>
2020-09-25 00:10:07 -0500

    scaling attempt 1

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 /* Sergio Perez-Valentin */
 /* September 24, 2020 */
+
+function getWidth() {
+    document.documentElement.clientWidth
+}
+
+function getHeight() {
+    document.documentElement.clientHeight
+}
+
+timer = setInterval(function() {
+	var wid = (getWidth()/1770);
+	document.body.style.transform = 'scale(' + getWidth()/1770 + ')';
+	document.body.style['-o-transform'] = 'scale(' + document.documentElement.clientWidth/1770 + ')';
+	document.body.style['-webkit-transform'] = 'scale(' + document.documentElement.clientWidth/1770 + ')';
+	document.body.style['-moz-transform'] = 'scale(' + document.documentElement.clientWidth/1770 + ')';
+}, 200);
