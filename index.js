bd3f6f1db230d94902fba458a694c65d9f303ec5
Sergio Perez-Valentin <sergiopv02@gmail.com>
2020-09-25 03:01:25 -0500

    added script icon rotation

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 /* Sergio Perez-Valentin */
 /* September 24, 2020 */
 
-function getWidth() {
-    document.documentElement.clientWidth
-}
 
-function getHeight() {
-    document.documentElement.clientHeight
-}
+scriptImages = ["./assets/ADA_Formatter.png", "./assets/Braille_Formatter.png", "./assets/Cap_Height.png",
+				"./assets/Graphic_Layout_Dataset.png", "./assets/Graphic_Layout_Grid.png", 
+				"./assets/Grid_Builder.png", "./assets/Sign_Creator.png", "./assets/Text_Spacer.png"]
+var num = 0;
+
 
 timer = setInterval(function() {
-	var wid = (getWidth()/1770);
-	document.body.style.transform = 'scale(' + getWidth()/1770 + ')';
+	var randScriptImage = scriptImages[num];
+	document.getElementById("scriptImg").src = randScriptImage;
+	num += 1;
+	if (num >= scriptImages.length) {
+		num = 0;
+	}
+
+	document.body.style.transform = 'scale(' + document.documentElement.clientWidth/1770 + ')';
 	document.body.style['-o-transform'] = 'scale(' + document.documentElement.clientWidth/1770 + ')';
 	document.body.style['-webkit-transform'] = 'scale(' + document.documentElement.clientWidth/1770 + ')';
 	document.body.style['-moz-transform'] = 'scale(' + document.documentElement.clientWidth/1770 + ')';
-}, 200);
+
+	/*
+	document.body.style.transform = 'scaleX(' + document.documentElement.clientWidth/screen.width + ') scaleY(' + document.documentElement.clientHeight/screen.height + ')';
+	document.body.style['-o-transform'] = 'scaleX(' + document.documentElement.clientWidth/screen.width + ') scaleY(' + document.documentElement.clientHeight/screen.height + ')';
+	document.body.style['-webkit-transform'] = 'scaleX(' + document.documentElement.clientWidth/screen.width + ') scaleY(' + document.documentElement.clientHeight/screen.height + ')';
+	document.body.style['-moz-transform'] = 'scaleX(' + document.documentElement.clientWidth/screen.width + ') scaleY(' + document.documentElement.clientHeight/screen.height + ')';
+	*/
+}, 1500);
