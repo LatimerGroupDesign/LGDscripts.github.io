129751d2a1ceee5dad20c48b6e79fbf9dc93612e
Sergio Perez-Valentin <sergiopv@iastate.edu>
2020-10-26 19:43:11 -0500

    Website Reset

--- index.js
+++ index.js
@@ -2,27 +2,58 @@
 /* September 24, 2020 */
 
 
-scriptImages = ["./assets/ADA_Formatter.png", "./assets/Braille_Formatter.png", "./assets/Cap_Height.png",
-				"./assets/Graphic_Layout_Dataset.png", "./assets/Graphic_Layout_Grid.png", 
-				"./assets/Grid_Builder.png", "./assets/Sign_Creator.png", "./assets/Text_Spacer.png"]
+scriptImages = ["./assets/ADA_Formatter_Grey.png", 
+				"./assets/Braille_Formatter_Grey.png", 
+				"./assets/Cap_Height_Grey.png",
+				"./assets/Graphic_Layout_Dataset_Grey.png", 
+				"./assets/Graphic_Layout_Grid_Grey.png", 
+				"./assets/Grid_Builder_Grey.png", 
+				"./assets/Sign_Creator_Grey.png", 
+				"./assets/Text_Spacer_Grey.png"]
 
-var cycleNum = 0;
+var cycleNum = 1;
 scriptImgCycle = setInterval(function() {
 	document.getElementById("scriptImg").src = scriptImages[cycleNum];
-	(um >= scriptImages.length) ? cycleNum = 0 : cycleNum += 1;
+	(cycleNum >= scriptImages.length-1) ? cycleNum = 0 : cycleNum += 1;
 }, 2000);
 
 
 scaling = setInterval(function() {
-	if (document.documentElement.clientWidth/1792 < document.documentElement.clientHeight/1040) {
-		document.body.style.transform = 'scale(' + document.documentElement.clientWidth/1792 + ')';
-		document.body.style['-o-transform'] = 'scale(' + document.documentElement.clientWidth/1792 + ')';
-		document.body.style['-webkit-transform'] = 'scale(' + document.documentElement.clientWidth/1792 + ')';
-		document.body.style['-moz-transform'] = 'scale(' + document.documentElement.clientWidth/1792 + ')';
+
+	document.body.style.transformOrigin = "0% 0% 0";
+	document.body.style.transition = "";
+
+	let widthRatio = document.documentElement.clientWidth/1792;
+	let heightRatio = document.documentElement.clientHeight/1040;
+	if (widthRatio < heightRatio) {
+		document.body.style.transform = 'scale(' + widthRatio + ')';
+		document.body.style['-o-transform'] = 'scale(' + widthRatio + ')';
+		document.body.style['-webkit-transform'] = 'scale(' + widthRatio + ')';
+		document.body.style['-moz-transform'] = 'scale(' + widthRatio + ')';
 	} else {
-		document.body.style.transform = 'scale(' + document.documentElement.clientHeight/1040 + ')';
-		document.body.style['-o-transform'] = 'scale(' + document.documentElement.clientHeight/1040 + ')';
-		document.body.style['-webkit-transform'] = 'scale(' + document.documentElement.clientHeight/1040 + ')';
-		document.body.style['-moz-transform'] = 'scale(' + document.documentElement.clientHeight/1040 + ')';
+		document.body.style.transform = 'scale(' + heightRatio + ')';
+		document.body.style['-o-transform'] = 'scale(' + heightRatio + ')';
+		document.body.style['-webkit-transform'] = 'scale(' + heightRatio + ')';
+		document.body.style['-moz-transform'] = 'scale(' + heightRatio + ')';
 	}
-}, 20);
+}, 100);
+
+function contentChange(page) {
+	strength = 1;
+	timer = setInterval(function() {
+		document.body.style.opacity = strength -= .01;
+	}, 20);
+	setTimeout(function(){ window.location.href = page; }, 2000);
+}
+
+function profilePopup() {
+	document.getElementById("profileCloseButton").style.display = 'block';
+	document.getElementById("profileTextInformation").style.display = 'block';
+	document.getElementById("plate7").style.transform = 'scale(2.3) rotate(0deg) translate(-100px,-50px)';
+}
+
+function profilePopdown() {
+	document.getElementById("profileCloseButton").style.display = 'none';
+	document.getElementById("profileTextInformation").style.display = 'none';
+	document.getElementById("plate7").style.transform = 'scale(1) rotate(-45deg) translate(-10px,-130px)';
+}
